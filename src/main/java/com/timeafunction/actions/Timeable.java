c583dd9455e79a0a6a2e6cb4d9899104c137bf51
RahulBaphana <rahul.kbaphana@yahoo.com>
2019-02-03 23:13:29 +0530

    remove Timeable interface

--- src/main/java/com/timeafunction/actions/Timeable.java
+++
@@ -1,9 +0,0 @@
-package com.timeafunction.actions;
-
-/**
- * Any function that is capable of being timed
- * @param <T> is the type of result returned after a function is timed
- */
-public interface Timeable<T> {
-    T time();
-}
