94dd40f7c12c26546147d07933e6621208b79a42
RahulBaphana <rahul.kbaphana@yahoo.com>
2019-05-12 12:53:30 +0530

    [RahulB/Mausam] Fix scripts and add verify .travis folder

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,9 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    echo "-K $encrypted_e4e4268fce40_key"
-    echo "-iv $encrypted_e4e4268fce40_iv"
-    echo "-in ${ENCRYPTED_GPG_KEY_LOCATION}"
-    echo "-out ${GPG_KEY_LOCATION}"
     openssl aes-256-cbc -K $encrypted_e4e4268fce40_key -iv $encrypted_e4e4268fce40_iv -in ${ENCRYPTED_GPG_KEY_LOCATION} -out ${GPG_KEY_LOCATION} -d
 fi
