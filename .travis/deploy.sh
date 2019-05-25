3319323ecc542e8e2d311856df1a9446a93c9437
RahulBaphana <rahul.kbaphana@yahoo.com>
2019-05-25 21:44:58 +0530

    [RahulB/Mausam] Fix signarchives

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,7 @@ then
 	if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
 	then
 		echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
+		./gradlew assemble -x signArchives
 		./gradlew publishToNexusAndClose
 	fi
 fi
