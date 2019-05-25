89b55eecf581512ebc2371f93f6de491f396e195
RahulBaphana <rahul.kbaphana@yahoo.com>
2019-05-25 19:42:25 +0530

    [RahulB/Mausam] Fix openssl command

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    openssl aes-256-cbc -K $encrypted_e4e4268fce40_key -iv $encrypted_e4e4268fce40_iv -in ${ENCRYPTED_GPG_KEY_LOCATION} -out ${GPG_KEY_LOCATION} -d
+    openssl aes-256-cbc -K $encrypted_f4d7fdf5335f_key -iv $encrypted_f4d7fdf5335f_iv -in ${ENCRYPTED_GPG_KEY_LOCATION} -out ${GPG_KEY_LOCATION} -d
 fi
