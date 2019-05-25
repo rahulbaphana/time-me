7dfd9c322399631de76324ed5f09e5c1c437ab52
RahulBaphana <rahul.kbaphana@yahoo.com>
2019-05-25 21:03:43 +0530

    [RahulB/Mausam] Dont archive

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,6 @@ then
 	if [ "$MANUAL_RELEASE_TRIGGERED" = "true" ];
 	then
 		echo "Sign, Upload archives to local repo, Upload archives to Sonatype, Close and release repository."
-		./gradlew uploadArchives publishToNexusAndClose
+		./gradlew publishToNexusAndClose
 	fi
 fi
